993ac2e267534de03efbfa2e33480c88c3c3c748
gonzaliski viski <gon.mendez13@gmail.com>
2021-10-13 13:26:13 -0300

    Resuelto el desafio

--- index.js
+++ index.js
@@ -25,6 +25,32 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function removeChildren(parent){
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function createNewElements(){
+  const ulEl = document.querySelector('ul')
+ 
+  const ElResult = cosasQueAprendimos.forEach((p)=>{
+
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = p.tema;
+    if(p.class){ newLiEl.classList.add(p.class) }
+    ulEl.appendChild(newLiEl);
+    
+  })
+}
+
+
+function main() {
+  const ulEl = document.querySelector('ul')
+  removeChildren(ulEl);
+ 
+  createNewElements();
+
+}
 
 main();
